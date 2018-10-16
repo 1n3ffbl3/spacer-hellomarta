06a14866e7643ef9070fa867f863f1a8829342a1
1n3ffbl3 <mizgier.m@gmail.com>
2018-10-16 20:58:39 +0200

    Add Claim.vue file

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,14 @@
 
 <style lang="scss" scoped>
 
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,800');
+  
+  * {
+  box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat' sans-serif;
+  }
+
 </style>
