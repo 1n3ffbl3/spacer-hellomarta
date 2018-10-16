7c736d4a55859916d89673029f72c5e939b00686
1n3ffbl3 <mizgier.m@gmail.com>
2018-10-16 20:58:39 +0200

    Add Claim.vue file

---
+++ src/components/Claim.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        <h1 class="claim">SPACER</h1>
+        <p class="subclaim">Begin your journey...
+        </p>      
+        <p class="subclaim">Type anything space-related to start.
+        </p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Claim"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        aling-items: center;
+    }
+
+</style>
