cefe31c49e995fe20175d17beeabe9d4aef750b4
1n3ffbl3 <mizgier.m@gmail.com>
2018-10-16 22:53:12 +0200

    Add props

--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -21,16 +21,30 @@
         flex-direction: column;
         justify-content: center;
         aling-items: center;
+        color: white;
     }
 
     .claim {
         font-size: 50px;
         margin: 0;
+
+
+        @media (min-width: 768px) {
+            font-size: 80px;
+        }
     }
 
     .subclaim {
         margin: 20px 0 0 0;
         font-size: 13px;
+        font-weight: 400;
+
+        @media (min-width: 768px) {
+            font-size: 17px;
+            width: 500px;
+            line-height: 1;
+            letter-spacing: 1px;
+        }
     }
 
 </style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,36 +1,51 @@
 <template>
-    <div class="searchWrapper">
         <input
                 id="search"
                 name="search"
-                v-model="searchValue"
-                @input="handleInput"
+                :value="value"
+                @input="handleChange"
         />
-    </div>
+
 </template>
 
 <script>
     export default {
-        name: "SearchInput"
+        name: "SearchInput",
+        props:{
+            value:{
+                type: String,
+                required: true,
+            },
+        },
+        methods: {
+            handleChange(e) {
+                this.$emit('input', e.target.value);
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-    .searchWrapper {
+    input {
         margin-top: 50px;
         display: flex;
-        flex-direction: column;
-        width: 300px;
-    }
-
-    label {
-        font-family: Montserrat, sans-serif;
-    }
-
-    input {
+        color: white;
+        text-align: center;
+        font-size: 18px;
+        font-weight: 300;
         height: 30px;
         border: 0;
         background: none;
-        border-bottom: 1px solid black;
+        border-bottom: 1px solid white;
+        transition: box-shadow .3s ease-out;
+
+        @media (min-width: 1024px) {
+            font-weight: 400;
+        }
+    }
+
+    input:focus {
+        outline: none;
+        box-shadow: 0 10px 20px -8px rgba(255, 255, 255, .5);
     }
 </style>
