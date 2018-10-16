4c518c3754a6061ffa2b15f951fcd408262b24a1
1n3ffbl3 <mizgier.m@gmail.com>
2018-10-16 22:53:12 +0200

    Add props

--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -1,26 +1,48 @@
 <template>
-    <div class="wrapper">
-        <h1 class="claim">SPACER</h1>
-        <p class="subclaim">Begin your journey...
-        </p>      
-        <p class="subclaim">Type anything space-related to start.
-        </p>
-    </div>
+  <div class="wrapper">
+    <h1 class="claim">SPACER</h1>
+    <p class="subclaim">Begin your journey...
+    </p>
+    <p class="subclaim">Type anything space-related to start.
+    </p>
+  </div>
 
 </template>
 
 <script>
-    export default {
-        name: "Claim"
-    }
+export default {
+  name: "Claim"
+};
 </script>
 
 <style lang="scss" scoped>
-    .wrapper {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        aling-items: center;
-    }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  aling-items: center;
+  color: white;
+}
 
+.claim {
+  font-size: 50px;
+  margin: 0;
+
+  @media (min-width: 768px) {
+    font-size: 80px;
+  }
+}
+
+.subclaim {
+  margin: 20px 0 0 0;
+  font-size: 13px;
+  font-weight: 400;
+
+  @media (min-width: 768px) {
+    font-size: 17px;
+    width: 500px;
+    line-height: 1;
+    letter-spacing: 1px;
+  }
+}
 </style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <input
+    id="search"
+    name="search"
+    :value="value"
+    @input="handleChange"
+  />
+
+</template>
+
+<script>
+export default {
+  name: "SearchInput",
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+input {
+  margin-top: 50px;
+  display: flex;
+  color: white;
+  text-align: center;
+  font-size: 18px;
+  font-weight: 300;
+  height: 30px;
+  border: 0;
+  background: none;
+  border-bottom: 1px solid white;
+  transition: box-shadow 0.3s ease-out;
+
+  @media (min-width: 1024px) {
+    font-weight: 400;
+  }
+}
+
+input:focus {
+  outline: none;
+  box-shadow: 0 10px 20px -8px rgba(255, 255, 255, 0.5);
+}
+</style>
