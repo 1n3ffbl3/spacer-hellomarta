edecd8a061e4e942c227b3f02248ceaec1bda034
1n3ffbl3 <mizgier.m@gmail.com>
2019-02-08 23:39:41 +0100

    Creating new components & improving page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-    <div class="wrapper">
-        <HeroImage/>
-        <Claim/>
-        <SearchInput v-model="searchValue" @input="handleInput"/>
+    <div :class="[{flexStart: step === 1}, 'wrapper']">      
+        <transition name="slide">
+            <img src="./assets/logo.svg" class="logo" v-if="step === 1">
+        </transition>
+        <transition name="fade">
+            <HeroImage v-if="step === 0"/>
+        </transition>
+        <Claim v-if="step === 0"/>
+        <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+        <div class="results" v-if="results && !loading && step === 1">
+            <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)" />
+        </div>
+        <div class="loader" v-if="step === 1 && loading"></div>
+        <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false"/>
     </div>
 </template>
 
@@ -11,7 +21,9 @@
     import debounce from 'lodash.debounce';
     import Claim from '@/components/Claim.vue';
     import SearchInput from '@/components/SearchInput.vue';
-    import HeroImage from "@/components/HeroImage.vue";
+    import HeroImage from '@/components/HeroImage.vue';
+    import Item from '@/components/Item.vue';
+    import Modal from '@/components/Modal.vue';
 
     const API = 'https://images-api.nasa.gov/search';
     export default {
@@ -20,20 +32,33 @@
             HeroImage,
             Claim,
             SearchInput,
+            Item,
+            Modal,
         },
         data() {
             return {
+                modalOpen: false,
+                modalItem: null,
+                loading: false,
+                step:0,
                 searchValue: '',
                 results: []
             };
         },
         methods: {
+            handleModalOpen(item) {
+                this.modalOpen = true;
+                this.modalItem = item;
+            },
             // eslint-disable-next-line
             handleInput: debounce(function () {
+                this.loading = true;
                 console.log(this.searchValue);
                 axios.get(`${API}?q=${this.searchValue}&media_type=image`)
                     .then((response) => {
                         this.results = response.data.collection.items;
+                        this.loading = false;
+                        this.step = 1;
                     })
                     .catch((error) => {
                         console.log(error);
@@ -65,6 +90,22 @@
         padding: 0;
     }
 
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .3s ease;
+    }
+
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+
+    .slide-enter-active, .slide-leave-active {
+        transition: margin-top .3s ease;
+    }
+
+    .slide-enter, .slide-leave-to {
+        margin-top: -50px;
+    }
+
     .wrapper {
         margin: 0;
         position: relative;
@@ -75,6 +116,67 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
+
+        &.flexStart {
+            justify-content: flex-start;
+        }
     }
 
+    .loader {
+        margin-top: 100px;
+        display: inline-block;
+        position: relative;
+        width: 64px;
+        height: 64px;
+
+        @media (min-width: 768px) {
+            width: 90px;
+            height: 90px;
+        }
+    }
+
+    .loader:after {
+        content: " ";
+        display: block;
+        border-radius: 50%;
+        width: 0;
+        height: 0;
+        margin: 6px;
+        box-sizing: border-box;
+        border: 26px solid #1e3d4a;
+        border-color: #1e3d4a transparent #1e3d4a transparent;
+        animation: loading 1.2s infinite;
+    }
+
+    @keyframes loading {
+        0% {
+            transform: rotate(0);
+            animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+        }
+        50% {
+            transform: rotate(900deg);
+            animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+        }
+        100% {
+            transform: rotate(1800deg);
+        }
+    }
+
+
+    .logo {
+        position: absolute;
+        top: 30px;
+    }
+
+    .results {
+        margin-top: 50px;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+
+        @media (min-width: 768px) {
+            width: 90%;
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
 </style>
